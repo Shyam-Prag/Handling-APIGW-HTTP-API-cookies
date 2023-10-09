e5fa1cb55750c2ec2a41c7ef7e6aab1544d6e612
Shyam Prag <76562906+Shyam-Prag@users.noreply.github.com>
2023-10-09 14:01:03 +0200

    Update handle_http_api_stripping_double_quotes.py

--- handle_http_api_stripping_double_quotes.py
+++ handle_http_api_stripping_double_quotes.py
@@ -3,7 +3,7 @@ import json
 def lambda_handler(event, context):
     #replicates HTTP API stripping double quotes
     input1_headers = {
-    'cookie': 'csrftoken=WvWLrENu05ETxo8ywkCOb9b8alpVAMPk6WOg3fhk8apYu7wMJz62MiPWNYnnFzYL; sessionid=35lghopnbktufyb8ulkhje0hn4uqc7m7; messages=60fd922b0ef183fb4a27cb1e853840d1735e3acb$[[\"__json_message\"\0540\05430\054\"Items must be selected in order to perform actions on them. No items have been changed.\"]]',
+    'cookie': 'csrftoken=WvWLrENu05ETxo8ywkCOb9b8xxxxasdasdasd123242423423424; sessionid=35lg1231231dsktufyb8ulkhje0hn4uqc7m7; messages=60fd922b12313131231231231312adsasdcb$[[\"__json_message\"\0540\05430\054\"Items must be selected in order to perform actions on them. No items have been changed.\"]]',
     }
     
     #replicates REST API cookie payload
